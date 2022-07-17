cd07081d5c653b4f18383a8d5cad4e30e8d2a0ce
Geobrain <43826167+Geobrain@users.noreply.github.com>
2022-07-17 11:35:43 +0300

    Update GameData.cs

--- GameData.cs
+++ GameData.cs
@@ -3,71 +3,71 @@ using System.Runtime.CompilerServices;
 using Unity.IL2CPP.CompilerServices;
 
 
-internal static partial class GameData
+namespace AppData
 {
-  internal static ArrayGameDates gameData = new ();
-}
-
-
-public sealed class ArrayGameDates
-{
-  public ObjectData[] array;
-  public int length;
-
-  public ref ObjectData this[int index]
+  public static class GameDate
   {
+    public static ArrayGameDates gameData = new();
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    get => ref array[index];
-  }
-
-  public ArrayGameDates(int cap = 0)
-  {
-    array = new ObjectData[cap > 0 ? cap : 5];
-    length = 0;
-  }
-  
-  [Il2CppSetOption(Option.NullChecks | Option.ArrayBoundsChecks, false)]
-  public void Add<T>(string key) where T : new()
-  {
-    if (length >= array.Length)
+    public static object Data(this string key)
     {
-      Array.Resize(ref array, length << 1);
-    }
-    array[length++] = new ObjectData(key, new T());
-  }
-}
-
-
-[Serializable]
-public class ObjectData : IEquatable<string>
-{
-  public string key;
-  public object obj;
-
-  public ObjectData(string key, object obj)
-  {
-    this.key = key;
-    this.obj = obj;
-  }
-
-  public bool Equals(string key) => this.key == key;
-}
-
-
-public static partial class DataHelper
-{
-  [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  private static object Data(this string key)
-  {
-    for (var index = 0; index < GameData.gameData.array.Length; index++)
-    {
-      var data = GameData.gameData.array[index];
-      if (data.Equals(key))
+      for (var index = 0; index < gameData.array.Length; index++)
       {
-        return data.obj;
+        var data = gameData.array[index];
+        if (data.Equals(key))
+        {
+          return data.obj;
+        }
       }
+
+      return null;
+    }
+  }
+
+
+  public sealed class ArrayGameDates
+  {
+    public ObjectData[] array;
+    public int length;
+
+    public ref ObjectData this[int index]
+    {
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      get => ref array[index];
     }
 
-    return null;
+    public ArrayGameDates(int cap = 0)
+    {
+      array = new ObjectData[cap > 0 ? cap : 5];
+      length = 0;
+    }
+
+    [Il2CppSetOption(Option.NullChecks | Option.ArrayBoundsChecks, false)]
+    public void Add<T>(string key) where T : new()
+    {
+      if (length >= array.Length)
+      {
+        Array.Resize(ref array, length << 1);
+      }
+
+      array[length++] = new ObjectData(key, new T());
+    }
+  }
+
+
+  [Serializable]
+  public class ObjectData : IEquatable<string>
+  {
+    public string key;
+    public object obj;
+
+    public ObjectData(string key, object obj)
+    {
+      this.key = key;
+      this.obj = obj;
+    }
+
+    public bool Equals(string key) => this.key == key;
   }
 }
